8722f579821bc342b4014f20dc9d3536cc8e6540
harley <arleycabralesvargas@gmail.com>
2025-09-16 09:08:39 -0500

    feat: :sparkles: Day seven JWT

--- Dia7/index.js
+++ Dia7/index.js
@@ -1,10 +1,15 @@
-import express  from "express";
-const app = express();
-const port = 3000; // Puedes usar cualquier puerto
+import express from "express";
+import dotenv from "dotenv";
+import routes from "./views/userRoutes.js";
 
-// Middleware para servir archivos estáticos
-app.use(express.static('public'));
+dotenv.config();
 
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
-});   
+const app= express();
+app.use(express.json());//Middleware para que solo acepte JSON'S
+
+//Rutas Principales
+app.use('/api',routes);
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log("FUNCIONAAAAAA EN:"+PORT);
+})
